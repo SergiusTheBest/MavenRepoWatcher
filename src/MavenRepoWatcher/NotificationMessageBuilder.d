b4146de9362fe5e4b197593a5f9480b5b0f72a17
Sergey Podobry <sergey.podobry@gmail.com>
2016-02-08 21:51:50 +0200

    Improve message formatting.

--- src/MavenRepoWatcher/NotificationMessageBuilder.d
+++ src/MavenRepoWatcher/NotificationMessageBuilder.d
@@ -2,7 +2,7 @@ class NotificationMessageBuilder
 {
     public NotificationMessageBuilder addNotify(in string packageUrl, in string knownVersion, in string newVersion)
     {
-        text ~= packageUrl ~ ": " ~ knownVersion ~ " => " ~ newVersion ~ "\n";
+        text ~= "* " ~ packageUrl ~ ": " ~ knownVersion ~ " => " ~ newVersion ~ "\n";
         return this;
     }
 
@@ -13,7 +13,7 @@ class NotificationMessageBuilder
 
     public string build()
     {
-        return isEmpty() ? "No packages have been updated." : "The following packages have been updated:\n" ~ text;
+        return isEmpty() ? "No packages have been updated." : "The following packages have been updated:\n\n" ~ text;
     }
 
     private string text;
